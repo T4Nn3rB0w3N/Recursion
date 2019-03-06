56b5375e8a89669dc055d7679c39f199fad94b1d
Bowen <tbow1382@740s-pl-120.local>
2019-03-06 09:47:29 -0700

    made a fibonacci sequence and made a factorial sequence

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -9,10 +9,42 @@
 #include <iostream>
 using namespace std;
 
+double fib(double number)
+{
+    if (number == 0 || number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(number - 1) + fib(number - 2);
+    }
+}
 
-
+double fact(double number)
+{
+    if (number == 0 || number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return number * fact(number - 1);
+    }
+}
 
 int main()
 {
+    cout << fact(100) << endl;
+    
+//    for (int num = 0; num < 20; num++)
+//    {
+//        cout << num << "th fibonacci value is " << fib(num) << endl;
+//        cout << num << " Factorial is: " << fact(num) << endl;
+//    }
+//    for (int number = 0; number < 10; number++)
+//    {
+//        cout << fib(number) << ", ";
+//    }
     return 0;
 }
